f8662355bd7159f43f1d414bae0979c99fcb2d58
rucha345 <73577591+rucha345@users.noreply.github.com>
2020-11-26 17:29:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+var helicopterimg, packageimg, helicopter, package;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterimg=loadImage("helicopter.png")
+	packageimg=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	package=createSprite(width/2, 80, 10,10);
+	package.addImage(packageimg);
+	package.scale=0.2;
+
+	helicopter=createSprite(width/2, 200, 10,10);
+	helicopter.addImage(helicopterimg);
+	helicopter.scale=0.6;
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color("green");
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 
+
+//creating box bodies
+boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleft=createSprite(boxPosition, boxY, 20,100);
+ 	boxleft.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxright=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxright.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+
+
+
+
+
+
+
+
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  package.x= packageBody.position.x 
+  package.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === LEFT_ARROW) {
+  
+	  helicopter.x=helicopter.x-20;    
+	  translation={x:-20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+  
+  
+	} else if (keyCode === RIGHT_ARROW) {
+	  helicopter.x=helicopter.x+20;
+	  translation={x:20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+	}
+	else if (keyCode === DOWN_ARROW) {
+	  Matter.Body.setStatic(packageBody,false);
+	  
+	}
+  }
